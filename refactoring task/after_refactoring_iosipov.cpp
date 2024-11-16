2e28c019a699bdf181219f6e60dda23c8f19b8d0
vovajim200 <adioshuesos@gmail.com>
2024-11-16 15:41:02 +0300

    cosmetics changes

--- refactoring task/after_refactoring_iosipov.cpp
+++ refactoring task/after_refactoring_iosipov.cpp
@@ -1,44 +1,45 @@
 ﻿#include <cmath>
 #include <iostream>
 
-const int ARRAY_SIZE = 10;
+
 const std::string INPUT_PROMPT = "Введите ";
 const std::string INCREASING_SEQUENCE_MESSAGE = "Последовательность возрастающая";
 const std::string DECREASING_SEQUENCE_MESSAGE = "Последовательность не возрастающая";
 
-void arrayInput(int array[], int size);
-bool isIncreasing(const int array[], int size);
+void intputArray(int arr[], int size);
+bool checkArrayIncreasing(const int arr[], int size);
 void outputResult(bool isIncreasing);
 
 int main() {
-    int array[ARRAY_SIZE];
+    const int arr_size = 10;
+    int arr[arr_size];
 
-    arrayInput(array, ARRAY_SIZE);
+    inputArray(arr, arr_size);
 
-    bool increasingSequence = isIncreasing(array, ARRAY_SIZE);
+    bool isIncreasing = checkArrayIncreasing(arr, arr_size);
 
-    outputResult(increasingSequence);
+    outputResult(isIncreasing);
 
     return 0;
 }
 
-void arrayInput(int array[], int size) {
+void inputArray(int arr[], const int size) {
     for (int i = 0; i < size; i++) {
         std::cout << INPUT_PROMPT << i + 1 << " элемент: ";
-        std::cin >> array[i];
+        std::cin >> arr[i];
     }
 }
 
-bool isIncreasing(const int array[], int size) {
+bool CheckArrayIncreasing(const int arr[], const int size) {
     for (int i = 0; i < size - 1; i++) {
-        if (array[i] > array[i + 1]) {
+        if (arr[i] > arr[i + 1]) {
             return false;
         }
     }
     return true;
 }
 
-void outputResult(bool isIncreasing) {
+void outputResult(const bool isIncreasing) {
     if (isIncreasing) {
         std::cout << INCREASING_SEQUENCE_MESSAGE << std::endl;
     } else {
