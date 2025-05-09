4221dad0b44ead1e8acd17c6bbc5701e0761a666
Ilya Osipov <adioshuesos@gmail.com>
2025-05-09 17:19:36 +0300

    start vector project

---
+++ multi-project project/projects/vector/test/test_vector.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+#include "vector.hpp"
+
+using oia::Vector;
+
+// Тесты
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
