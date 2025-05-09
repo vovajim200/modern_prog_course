9ee385ad0f3e037a9540e3db9d0c40bc8e91906a
Ilya Osipov <adioshuesos@gmail.com>
2025-05-09 18:37:31 +0300

    start doubly_linked_list project

---
+++ multi-project project/projects/doubly_linked_list/test/test_doubly_linked_list.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+#include "doubly_linked_list.hpp"
+
+using oia::DoublyLinkedList;
+
+// ТЕСТЫ
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
