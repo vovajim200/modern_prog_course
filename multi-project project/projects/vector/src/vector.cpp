4b59b207bf0f233265ed50b2a065f6ad3691f7e9
Ilya Osipov <adioshuesos@gmail.com>
2025-05-09 18:12:18 +0300

    completed vector

--- multi-project project/projects/vector/src/vector.cpp
+++ multi-project project/projects/vector/src/vector.cpp
@@ -4,42 +4,98 @@
 
 using oia::Vector;
 
+template <typename T>
+const std::size_t oia::Vector<T>::START_CAPACITY = 4;
+
 template<typename T>
 Vector<T>::Vector() {
-	// TODO
+	size = 0;
+	capacity = START_CAPACITY;
+	arr = new T[capacity];
 }
 
 template<typename T>
 Vector<T>::~Vector() {
-	// TODO
+	if (arr != nullptr) {
+		delete[] arr;
+		arr = nullptr;
+	}
+	capacity = 0;
+	size = 0;
 }
 
 template<typename T>
 std::size_t Vector<T>::get_size() const noexcept {
-	// TODO
+	return size;
 }
 
 template<typename T>
 bool Vector<T>::has_item(const T& value) const noexcept {
-	// TODO
+	for (std::size_t i = 0; i < size; ++i) {
+		if (arr[i] == value) {
+			return true;
+		}
+	}
+	return false;
 }
 
 template<typename T>
 bool Vector<T>::insert(const std::size_t position, const T& value) {
-	// TODO
+	if (position > size) {
+		return false;
+	}
+	if (size == capacity) {
+		capacity *= 2;
+		T* new_arr = new T[capacity];
+		for (std::size_t i = 0; i < size; ++i) {
+			new_arr[i] = arr[i];
+		}
+		delete[] arr;
+		arr = new_arr;
+	}
+	for (std::size_t i = size; i > position; --i) {
+		arr[i] = arr[i - 1];
+	}
+	arr[position] = value;
+	size++;
+	return true;
 }
 
 template<typename T>
 void Vector<T>::print() const noexcept {
-	// TODO
+	for (std::size_t i = 0; i < size; ++i) {
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
 }
 
 template<typename T>
 void Vector<T>::push_back(const T& value) {
-	// TODO
+	if (size == capacity) {
+		capacity *= 2;
+		T* new_arr = new T[capacity];
+		for (std::size_t i = 0; i < size; ++i) {
+			new_arr[i] = arr[i];
+		}
+		delete[] arr;
+		arr = new_arr;
+	}
+	arr[size] = value;
+	size++;
 }
 
 template<typename T>
 bool Vector<T>::remove_first(const T& value) {
-	// TODO
+	for (std::size_t i = 0; i < size; ++i) {
+		if (arr[i] == value) {
+			for (std::size_t j = i; j < size - 1; ++j) {
+				arr[j] = arr[j + 1];
+			}
+			size--;
+			return true;
+		}
+	}
+	return false;
 }
+
+template class oia::Vector<int>;
--- multi-project project/projects/vector/test/test_vector.cpp
+++ multi-project project/projects/vector/test/test_vector.cpp
@@ -4,7 +4,42 @@
 
 using oia::Vector;
 
-// Тесты
+TEST(VectorTest, DefaultConstructor) {
+	Vector<int> v;
+	EXPECT_EQ(v.get_size(), 0);
+}
+
+TEST(VectorTest, PushBack) {
+	Vector<int> v;
+	for (int i = 0; i < 10; ++i) {
+		v.push_back(i);
+		EXPECT_TRUE(v.has_item(i));
+	}
+	EXPECT_FALSE(v.has_item(-1));
+	EXPECT_FALSE(v.has_item(10));
+}
+
+TEST(VectorTest, Insert) {
+	Vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	v.insert(1, 3);
+	EXPECT_EQ(v.get_size(), 3);
+	EXPECT_TRUE(v.has_item(2));
+	EXPECT_FALSE(v.has_item(4));
+}
+
+TEST(VectorTest, RemoveFirst) {
+	Vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(4);
+	EXPECT_EQ(v.get_size(), 4);
+	EXPECT_TRUE(v.remove_first(1));
+	EXPECT_EQ(v.get_size(), 3);
+	EXPECT_FALSE(v.has_item(1));
+}
 
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
