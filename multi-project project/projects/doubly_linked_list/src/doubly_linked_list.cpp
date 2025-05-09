ed8d829f8aa2e27e99db5d94e6734509e0d7065b
Ilya Osipov <adioshuesos@gmail.com>
2025-05-09 19:33:39 +0300

    [doubly_linked_list] add push_back, sup. methods & tests

--- multi-project project/projects/doubly_linked_list/src/doubly_linked_list.cpp
+++ multi-project project/projects/doubly_linked_list/src/doubly_linked_list.cpp
@@ -6,30 +6,57 @@ using oia::DoublyLinkedList;
 
 template<typename T>
 DoublyLinkedList<T>::~DoublyLinkedList() {
-	// TODO
+	for (Node* current = begin; current != nullptr; ) {
+		Node* next = current->next;
+		delete current;
+		current = next;
+	}
+	begin = nullptr;
+	end = nullptr;
 }
 
 template<typename T>
 std::size_t DoublyLinkedList<T>::get_size() const noexcept {
-	// TODO
+	std::size_t size = 0;
+	for (Node* current = begin; current != nullptr; current = current->next) {
+		size++;
+	}
+	return size;
 }
 
 template<typename T>
 bool DoublyLinkedList<T>::has_item(const T& value) const noexcept {
-	// TODO
+	for (Node* current = begin; current != nullptr; current = current->next) {
+		if (current->value == value) {
+			return true;
+		}
+	}
+	return false;
 }
 
 template<typename T>
 void DoublyLinkedList<T>::print() const noexcept {
-	// TODO
+	for (Node* current = begin; current != nullptr; current = current->next) {
+		std::cout << current->value << " ";
+	}
 }
 
 template<typename T>
 void DoublyLinkedList<T>::push_back(const T& value) {
-	// TODO
+	Node* new_node = new Node(value);
+	if (begin == nullptr) {
+		begin = new_node;
+		end = new_node;
+	} else {
+		end->next = new_node;
+		new_node->prev = end;
+		end = new_node;
+	}
 }
 
 template<typename T>
 bool DoublyLinkedList<T>::remove_first(const T& value) noexcept {
 	// TODO
 }
+
+template class DoublyLinkedList<int>;
--- multi-project project/projects/doubly_linked_list/test/test_doubly_linked_list.cpp
+++ multi-project project/projects/doubly_linked_list/test/test_doubly_linked_list.cpp
@@ -4,7 +4,22 @@
 
 using oia::DoublyLinkedList;
 
-// ТЕСТЫ
+TEST(DoublyLinkedListTests, DefaultConstructor) {
+	DoublyLinkedList<int> list;
+	EXPECT_EQ(list.get_size(), 0);
+	EXPECT_FALSE(list.has_item(1));
+}
+
+TEST(DoublyLinkedListTests, PushBack) {
+	DoublyLinkedList<int> list;
+	for (int i = 0; i < 100; ++i) {
+		list.push_back(i);
+		EXPECT_TRUE(list.has_item(i));
+	}
+	EXPECT_EQ(list.get_size(), 100);
+	EXPECT_FALSE(list.has_item(-1));
+	EXPECT_FALSE(list.has_item(101));
+}
 
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
